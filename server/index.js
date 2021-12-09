71fbd0ff22ca99b3b75c4eaa3f4c20cc737369c9
Khristian Lopez <khristianlopez@Khristians-MacBook-Air.local>
2021-12-09 11:05:17 -0800

    Added getAllTopics route, connection string

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = 3000;
+const db = require('./controller.js')
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/topics', (req, res) => {
+  db.getAllTopics((err, response) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(response)
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
